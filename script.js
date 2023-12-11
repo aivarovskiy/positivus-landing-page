ab58cd410833e2e557f0ef6e32205148ca68eb8f
aivarovsky <ivarovskiy@yandex.ru>
2023-12-11 16:43:22 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,42 @@
+const nav = document.getElementById("nav");
+const navToggle = document.getElementById("nav-toggle");
+
+const form = document.getElementById('newsletter-form');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    form.reset();
+});
+
+visualViewport.addEventListener("resize", () => {
+    if (getWidth() < 768){
+        nav.style.display = "block";
+        if (!navToggle.checked){
+            nav.style.display = "none";
+        }
+
+    }
+    else {
+        nav.style.display = "flex";
+        navToggle.checked = false;
+    }
+  });
+
+function toggleNav() {
+    if (getWidth() < 768){
+        nav.style.display = "block";
+        if (navToggle.checked){
+            nav.style.display = "none";
+        }
+    }
+}
+function closeNav() {
+    if (getWidth() < 768){
+        nav.style.display = "none";
+        navToggle.checked = false;
+    }
+}
+
+function getWidth() {
+    return Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
+}
